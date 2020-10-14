7c7f0b1d7fb20b48b8b51744d7eea59a681a5bf2
Jihoon Lim <jihoon.lim@pcc.edu>
2020-10-13 19:31:21 -0700

    Added main methi that prints hello world

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,4 +1,12 @@
 package edu.pcc.cis.helloworld;
 
+/**
+ * Printing Hello, World!
+ * @author: JihoonLim
+ * @version: 10/13/2020
+ */
 public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello, World!");
+    }
 }
